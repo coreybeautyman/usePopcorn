e062cdc579fa15029d5a2c84b8b63ee632205fa0
coreybeautyman <corey.beautyman@gmail.com>
2024-08-02 11:59:07 +0100

    fixing bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App-V2';
@@ -9,20 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// root.render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//     <StarRating
-//       maxRating={5}
-//       size={20}
-//       color={'red'}
-//       className={'test'}
-//       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-//       defaultRating={3}
-//     />
-//     <StarRating maxRating={10} defaultRating={7} />
-//     <StarRating maxRating={5} />
-//     <Test />
-//   </React.StrictMode>
-// );
--- src/startRating.js
+++ src/startRating.js
@@ -1,4 +1,4 @@
-import { startTransition, useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const containerStyle = {
