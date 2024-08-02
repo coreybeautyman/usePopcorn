b1455c630ce506f5d4fe7a86222c8154b203ed01
coreybeautyman <corey.beautyman@gmail.com>
2024-08-02 11:19:52 +0100

    master

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App-V2';
+import StarRating from './startRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating onSetMovieRating={setMovieRating} />
+      <p>This movie was rated {movieRating} Stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +22,19 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <StarRating
+//       maxRating={5}
+//       size={20}
+//       color={'red'}
+//       className={'test'}
+//       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+//       defaultRating={3}
+//     />
+//     <StarRating maxRating={10} defaultRating={7} />
+//     <StarRating maxRating={5} />
+//     <Test />
+//   </React.StrictMode>
+// );
---
+++ src/useMovies.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+const KEY = `a0430203`;
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(
+    function () {
+      //   callBack?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setError('');
+
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!res.ok) throw new Error('Something went wrong fetching movies');
+
+          const data = await res.json();
+
+          if (data.Response === 'False') throw new Error('Movie not found');
+
+          setMovies(data.Search);
+        } catch (err) {
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+            console.log(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError('');
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
